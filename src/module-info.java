e6795430c39f63060d78183d8e3975ff5903bfec
khati <khati@DESKTOP-B2D93F2>
2023-02-21 18:42:08 -0600

    checking

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author khati
+ *
+ */
+module myrepo {
+}
---
+++ src/myrepo/java.java
@@ -0,0 +1,11 @@
+package myrepo;
+
+public class java {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Testing GitLab");
+
+	}
+
+}
